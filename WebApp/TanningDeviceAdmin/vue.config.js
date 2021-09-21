65066c26747f2ba930dcfb1a1fabf555a22ce4fb
nauman3s <naumanshakir3s@gmail.com>
2021-09-21 11:46:55 +0500

    updated fe

--- WebApp/TanningDeviceAdmin/vue.config.js
+++ WebApp/TanningDeviceAdmin/vue.config.js
@@ -1,12 +1,9 @@
 // vue.config.js
 module.exports = {
     // options...
-    dev: {
-        proxyTable: {
-            '/v1': {
-               target: 'http://localhost:3000',
-               changeOrigin: true // or true depending on your needs
-            },
-        },
+    devServer: {
+        proxy: {
+            proxy: 'http://localhost:3000/',
+        }
     }
   }

f7a259f298242de014d8703105b845b9ff4f2685
nauman3s <naumanshakir3s@gmail.com>
2021-09-21 10:35:16 +0500

    updated frontend

--- WebApp/TanningDeviceAdmin/vue.config.js
+++ WebApp/TanningDeviceAdmin/vue.config.js
@@ -1,7 +1,8 @@
 // vue.config.js
 module.exports = {
-    // options...
-    devServer: {
-        proxy: 'http://127.0.0.1:3000/',
+    dev: {
+      proxyTable: {
+        "/": "http://127.0.0.1:3000/api/v1/loginAdmin"
+      }
     }
   }

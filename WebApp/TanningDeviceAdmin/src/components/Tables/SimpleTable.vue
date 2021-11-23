9da694fc9ae485d89a0cdcd8d14d404e756c3c39
Nauman3S <naumanshakir3s@gmail.com>
2021-11-23 15:59:46 +0500

    updated

--- WebApp/TanningDeviceAdmin/src/components/Tables/SimpleTable.vue
+++ WebApp/TanningDeviceAdmin/src/components/Tables/SimpleTable.vue
@@ -56,6 +56,10 @@ export default {
   },
   data() {
     return {
+      type: ["", "info", "success", "warning", "danger"],
+      notifications: {
+        topCenter: false,
+      },
       selected: [],
       macs: [
         {
